f14fb914ced05eb27b804d1897c4b776519a63db
Chito Salano <Alfonchitosalao2@gmail.com>
2016-08-13 10:03:13 +0800

    restaurant

---
+++ mathematics/fundamentals/restaurant.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace HackerRannk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n >= 1)
+            {                
+                string[] arr_temp = Console.ReadLine().Split(' ');
+                int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+                int gcd = GCD(arr[0], arr[1]);
+                int res = Math.Abs((arr[0] / gcd) * (arr[1] / gcd));
+                Console.WriteLine(res);               
+                n--;
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int tmp = b;
+                b = a % b;
+                a = tmp;
+            }
+
+            return a;
+        }
+
+
+    }
+}
